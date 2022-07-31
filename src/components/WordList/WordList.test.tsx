684e620384cf721ecb1a428cf3c35cb5b4b4b00b
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 17:44:23 -0600

    Red: Assert <WordList /> Shows Proper Order

--- src/components/WordList/WordList.test.tsx
+++ src/components/WordList/WordList.test.tsx
@@ -7,9 +7,9 @@ import { WordList } from './WordList';
 
 describe('<WordList />', () => {
     const words: [string, number][] = [
-        ['one', 1],
-        ['two', 2],
-        ['three', 3],
+        ['orange', 3],
+        ['pear', 2],
+        ['apple', 1],
     ];
     test('Renders an <li> for each item', () => {
         const { container } = render(<WordList words={words} />);
@@ -18,8 +18,16 @@ describe('<WordList />', () => {
 
     test("each <li> contains the word and it's count", () => {
         const { queryByText } = render(<WordList words={words} />);
-        expect(queryByText('one: 1')).toBeInTheDocument();
-        expect(queryByText('two: 2')).toBeInTheDocument();
-        expect(queryByText('three: 3')).toBeInTheDocument();
+        expect(queryByText('"orange" - (3 Times)')).toBeInTheDocument();
+        expect(queryByText('"pear" - (2 Times)')).toBeInTheDocument();
+        expect(queryByText('"apple" - (1 Times)')).toBeInTheDocument();
+    });
+
+    test('<li> are presented in the order they were received', () => {
+        const { queryByText } = render(<WordList words={words} />);
+
+        expect(queryByText('#1: "orange" - (3 Times)')).toBeInTheDocument();
+        expect(queryByText('#2: "pear" - (2 Times)')).toBeInTheDocument();
+        expect(queryByText('#3: "apple" - (1 Times)')).toBeInTheDocument();
     });
 });
