9d0be11d77cb1ec0fb5be3999b2a4d16cccbb55e
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 17:21:19 -0600

    Red: WordList component lists provided words

---
+++ src/components/WordList/WordList.test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+
+import { h } from 'preact';
+import { render } from '@testing-library/preact';
+
+import { WordList } from './WordList';
+
+describe('<WordList />', () => {
+    const words = [
+        ['one', 1],
+        ['two', 2],
+        ['three', 3],
+    ];
+    test('Renders an <li> for each item', () => {
+        const { container } = render(<WordList words={words} />);
+        expect(container.getElementsByTagName('li')).toHaveLength(words.length);
+    });
+
+    test("each <li> contains the word and it's count", () => {
+        const { queryByText } = render(<WordList words={words} />);
+        expect(queryByText('one: 1')).toBeInTheDocument();
+        expect(queryByText('two: 2')).toBeInTheDocument();
+        expect(queryByText('three: 3')).toBeInTheDocument();
+    });
+});
