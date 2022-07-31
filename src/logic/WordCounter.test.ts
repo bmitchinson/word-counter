17baf88b923ebc6a2dc54a158d4817ce9ae08dd4
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 11:40:13 -0600

    Red: WordCounter read file tests

---
+++ src/logic/WordCounter.test.ts
@@ -0,0 +1,21 @@
+import { WordCounter } from './WordCounter';
+
+const bookTestTxt = './src/test/text.test.txt';
+
+describe('WordCounter', () => {
+    it('Stores contents of a text file as keys in an object', () => {
+        const wordCounter = new WordCounter(bookTestTxt);
+        ['apple', 'orange', 'peach'].forEach((fruit) => {
+            expect(WordCounter.contents).toHaveProperty(fruit);
+        });
+    });
+
+    it("Throws an error if the provided text file doesn't exist", () => {
+        const badFileName = 'nope.nothing';
+        expect(() => new WordCounter(badFileName)).toThrowError(
+            new Error(`Invalid file "${badFileName}" provided to WordCounter`),
+        );
+    });
+
+    // todo: pass in ExcludeList to constructor
+});
