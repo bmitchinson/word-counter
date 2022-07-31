11dd848256dcc18390ae96e702a5abb3ee2e2329
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 12:12:16 -0600

    Green: WordCounter.get tests

--- src/logic/WordCounter.test.ts
+++ src/logic/WordCounter.test.ts
@@ -6,7 +6,10 @@ const excludeSet = new ExcludeSet(excludeTestTxt);
 
 describe('WordCounter', () => {
     it('Stores contents of a text file as keys in an object', () => {
-        const wordCounter = new WordCounter(bookTestTxt);
+        const wordCounter = new WordCounter({
+            textFileName: bookTestTxt,
+            excludeSet,
+        });
         ['apple', 'orange', 'peach'].forEach((fruit) => {
             expect(wordCounter.contents.has(fruit)).toBe(true);
         });
@@ -14,7 +17,9 @@ describe('WordCounter', () => {
 
     it("Throws an error if the provided text file doesn't exist", () => {
         const badFileName = 'nope.nothing';
-        expect(() => new WordCounter(badFileName)).toThrowError(
+        expect(
+            () => new WordCounter({ textFileName: badFileName, excludeSet }),
+        ).toThrowError(
             new Error(`Invalid file "${badFileName}" provided to WordCounter`),
         );
     });
@@ -23,9 +28,12 @@ describe('WordCounter', () => {
     // and in excludeset
 
     it('Excludes files contained in a provided ExcludeList', () => {
-        const wordCounter = new WordCounter(bookTestTxt, excludeSet);
+        const wordCounter = new WordCounter({
+            textFileName: bookTestTxt,
+            excludeSet,
+        });
         ['nail', 'hammer'].forEach((word) => {
-            expect(WordCounter.get(word)).toBe(false);
+            expect(wordCounter.get(word)).toBe(false);
         });
     });
 });
--- src/logic/WordCounter.ts
+++ src/logic/WordCounter.ts
@@ -1,11 +1,23 @@
+import { ExcludeSet } from './ExcludeSet';
 import { loadfile } from './loadfile';
+
+interface params {
+    textFileName: string;
+    excludeSet: ExcludeSet;
+}
 
 export class WordCounter {
     public contents = new Map<string, number>();
+    private excludeSet;
 
-    constructor(textFileName: string) {
+    constructor({ textFileName, excludeSet }: params) {
+        this.excludeSet = excludeSet;
         loadfile(textFileName, 'WordCounter', (line) => {
             this.contents.set(line, 0);
         });
+    }
+
+    get(word: string) {
+        return !this.excludeSet.includes(word) && this.contents.has(word);
     }
 }
