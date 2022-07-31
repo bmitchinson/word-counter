ff9f4cc338faf79a0488845775ea4ead590e315b
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 11:20:13 -0600

    Refactor: ExcludeList loads from file

--- src/logic/ExcludeMap.ts
+++ src/logic/ExcludeMap.ts
@@ -1,16 +1,17 @@
 import { readFileSync } from 'fs';
 
 export class ExcludeMap {
+    public contents = {};
+
     constructor(excludeFile: string) {
         try {
             const file = readFileSync(excludeFile, 'utf-8');
+            file.split(/\r?\n/).forEach((line) => {
+                this.contents[line] = undefined;
+            });
         } catch (e) {
+            // refactor: include attempted file in error message
             throw new Error('Invalid file provided to ExcludeMap');
         }
     }
-
-    public contents = {
-        nail: undefined,
-        hammer: undefined,
-    };
 }
