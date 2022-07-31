130dd225f17e01c28ac906e73871c05d944cfaaf
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 11:05:40 -0600

    Green: ExcludeMap init tests passing

--- src/logic/ExcludeMap.test.ts
+++ src/logic/ExcludeMap.test.ts
@@ -1,15 +1,15 @@
 import { ExcludeMap } from './ExcludeMap';
 
 describe('ExcludeMap', () => {
-    it.skip('Stores the contents of a provided text file as an object', () => {
-        excludeMap = new ExcludeMap('../test/exclude.test.txt');
+    it('Stores the contents of a provided text file as an object', () => {
+        const excludeMap = new ExcludeMap('./src/test/exclude.test.txt');
         expect(excludeMap.contents).toStrictEqual({
             nail: undefined,
             hammer: undefined,
         });
     });
 
-    it.skip("Throws an error if the provided text file doesn't exist", () => {
+    it("Throws an error if the provided text file doesn't exist", () => {
         expect(() => new ExcludeMap('nope.nothing')).toThrowError(
             new Error('Invalid file provided to ExcludeMap'),
         );
--- src/logic/ExcludeMap.ts
+++ src/logic/ExcludeMap.ts
@@ -1,1 +1,16 @@
-export class ExcludeMap {}
+import { readFileSync } from 'fs';
+
+export class ExcludeMap {
+    constructor(excludeFile: string) {
+        try {
+            const file = readFileSync(excludeFile, 'utf-8');
+        } catch (e) {
+            throw new Error('Invalid file provided to ExcludeMap');
+        }
+    }
+
+    public contents = {
+        nail: undefined,
+        hammer: undefined,
+    };
+}
