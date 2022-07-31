e5910e90fb856cde25c4001ff663fdaca19d6478
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 16:29:37 -0600

    Red: "e2e" test word counts found in Moby Dick

---
+++ src/test/e2e.test.ts
@@ -0,0 +1,29 @@
+import { excludeListFilePath, mobydickFilePath } from '../text_files/textfiles';
+import { WordCounter } from '../logic/WordCounter';
+import { ExcludeSet } from '../logic/ExcludeSet';
+import { wordRanker } from '../logic/wordranker';
+
+const ex = new ExcludeSet({ excludeFilePath: excludeListFilePath });
+const w = new WordCounter({
+    textFilePath: mobydickFilePath,
+    excludeSet: ex,
+});
+const finalResult = wordRanker({ words: w.contents, desiredResults: 30000 });
+
+const correctCounts = [
+    { word: 'tucked', count: 3 },
+    { word: 'accounts', count: 9 },
+    { word: 'joppa', count: 8 },
+];
+describe.each(correctCounts)(
+    'e2e tests to confirm manually confirmed counts from the Moby Dick text',
+    ({ word, count }) => {
+        it(`Confirms that "${word}" is found ${count} times`, () => {
+            expect(
+                finalResult.find((ele) => {
+                    return ele[0] === word && ele[1] === count;
+                }),
+            ).toBeTruthy();
+        });
+    },
+);
