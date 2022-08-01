af9a744d6c92f7d71c5bfcb82f643dbaa997c035
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-31 18:01:44 -0600

    Remove stale comment + unused component

--- src/test/text_files/testfilestest.ts
+++ src/test/text_files/testfilestest.ts
@@ -1,5 +1,2 @@
 export const bookTestTxt = './src/test/text_files/text.test.txt';
 export const excludeTestTxt = './src/test/text_files/exclude.test.txt';
-
-// refactor: update bookTestText to be delineated by spaces and new lines,
-//   not just new lines. Update "line" verbiage.
